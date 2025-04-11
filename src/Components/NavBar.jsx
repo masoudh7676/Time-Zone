8380b529e50855d13e04a39e61145591fd3ea539
Masoud Haseli <haselimasoud@gmail.com>
2025-04-11 18:32:15 +0330

    nav links creating

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -5,7 +5,37 @@ export default function NavBar() {
     <>
         <nav>
             <div>
-                <img src="./images/logo.png.webp" alt="" />
+                <img src="./images/logo.png.webp" alt="Time-Zone" />
+            </div>
+            <ul>
+                <li>
+                    <a href="">Home</a>
+                </li>
+                <li>
+                    <a href="">Shop</a>
+                </li>
+                <li>
+                    <a href="">About</a>
+                </li>
+                <li>
+                    <a href="">Latest</a>
+                </li>
+                <li>
+                    <a href="">Blog</a>
+                </li>
+                <li>
+                    <a href="">Pages</a>
+                </li>
+                <li>
+                    <a href="">Contact</a>
+                </li>
+            </ul>
+            <div>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
             </div>
         </nav>
     </>
