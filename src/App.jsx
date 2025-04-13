95cdf53fa76da417470032fd318a8859b46f35b9
Masoud Haseli <haselimasoud@gmail.com>
2025-04-13 23:45:14 +0330

    creating Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import NavBar from './Components/NavBar/NavBar'
+import Header from './Components/Header/Header'
 
 function App() {
 
   return (
       <>
         <NavBar/>
+        <Header/>
       </>
   )
 }
