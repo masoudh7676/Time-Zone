53a82022ae9032428877b5f2971d0e1374229f80
Masoud Haseli <haselimasoud@gmail.com>
2025-04-14 01:32:03 +0330

    Header basics

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div></div>
+    <div>
+        <div className='text-description'>
+            <h4 className='uppercase'>
+                select your new perfect style
+            </h4>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, sapiente. Aut vero aperiam voluptatibus culpa laudantium assumenda dolor!</p>
+            <button className='bg-[#4a4a4b]'>Shop Now</button>
+        </div>
+    </div>
   )
 }
