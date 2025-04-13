2e8c8283e812bba535353792cd467ef94c6fa8e4
Masoud Haseli <haselimasoud@gmail.com>
2025-04-14 01:59:08 +0330

    text styles

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
-
+import './Header.css'
 export default function Header() {
   return (
-    <div>
-        <div className='text-description'>
-            <h4 className='uppercase'>
+    <div className='flex items-center bg-[#f0f0f2] header '>
+        <div className='text-description  w-[50%]'>
+            <h3 className='uppercase large-text'>
                 select your new perfect style
-            </h4>
+            </h3>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, sapiente. Aut vero aperiam voluptatibus culpa laudantium assumenda dolor!</p>
             <button className='bg-[#4a4a4b]'>Shop Now</button>
         </div>
+        <div>
+                <img src="/images/watch.png.webp" alt="Time-Zone" />
+            </div>
     </div>
   )
 }
