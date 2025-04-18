86c540d24c6459bafee366b48e50128d3973284c
Masoud Haseli <haselimasoud@gmail.com>
2025-04-19 02:07:15 +0330

    main section start

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,7 +4,23 @@ import Header from '../Header/Header'
 export default function Home() {
   return (
     <>
-    <Header/>    
+    <Header/>   
+    <div className=''>
+      <div className='examples grid grid-cols-2 gap-54'>
+        <div>
+          <img src="./images/rolex1.jpg" className='w-sm' alt="" />
+        </div>
+        <div>
+          <img src="./images/citizen.jpeg" className='w-sm' alt="" />
+        </div>
+        <div>
+          <img src="./images/casio.webp" className='w-sm' alt="" />
+        </div>
+        <div>
+          <img src="./images/swatch.jpg" className='w-sm' alt="" />
+        </div>
+      </div>
+    </div>
     </>
   )
 }
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -33,50 +33,52 @@ export default function NavBar() {
                     </div>
                     {/* Nav */}
                     <ul className='flex w-[100%] justify-center gap-7'>
-                        <li><Link to="/home" className = 'hover-effect'>Home</Link></li>
-                        <li><Link to="shop" className = 'hover-effect'>Shop</Link></li>
-                        <li><Link to="about" className = 'hover-effect'>About</Link></li>
-                        <li>
-                            <a href="#" className='relative hover-effect' >Latest <span className='absolute red-btn rounded-2xl text-white shadow'>Hot</span></a>
+                        <li><Link to="/home" className='hover-effect'>Home</Link></li>
+                        <li><Link to="shop" className='hover-effect'>Shop</Link></li>
+                        <li><Link to="about" className='hover-effect'>About</Link></li>
+                        <li className='relative action red__btn'>
+                            <a href="#" className='relative hover-effect with-sub'>Latest</a>
                             {/* Sub Menu */}
-                            <ul className='hidden'>
-                                <li>Product List</li>
-                                <li>Product Details</li>
+                            <div className='hidden absolute cursor-pointer w-[290%] rounded-sm bg-white latest__sub'>
+                            <ul>
+                                <li className='arrow hover-effect'><a href="">Product List</a> </li>
+                                <li className='hover-effect'><a href="">Product Details</a> </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li className='action'>
+                            <Link to="blog" className='hover-effect with-sub'>Blog</Link>
+                            {/* Sub Menu */}
+                            <ul className='absolute hidden cursor-pointer rounded-sm bg-white latest__sub'>
+                                <li className='arrow hover-effect'><a href="">Blog</a></li>
+                                <li className='hover-effect'><a href="">Blog Details</a></li>
                             </ul>
                         </li>
-                        <li>
-                            <Link to="blog" className = 'hover-effect'>Blog</Link>
+                        <li className='action'>
+                            <a href="#" className='hover-effect with-sub'>Pages</a>
                             {/* Sub Menu */}
-                            <ul className='hidden'>
-                                <li>Blog</li>
-                                <li>Blog Details</li>
+                            <ul className='absolute hidden cursor-pointer rounded-sm bg-white latest__sub'>
+                                <li className='hover-effect'>Login</li>
+                                <li className='hover-effect'>Cart</li>
+                                <li className='hover-effect'>Element</li>
+                                <li className='hover-effect'>Confirmation</li>
+                                <li className='hover-effect'>Product Checkout</li>
                             </ul>
                         </li>
-                        <li>
-                            <a href="#" className='hover-effect'>Pages</a>
-                            {/* Sub Menu */}
-                            <ul className='hidden'>
-                                <li>Login</li>
-                                <li>Cart</li>
-                                <li>Element</li>
-                                <li>Confirmation</li>
-                                <li>Product Checkout</li>
-                            </ul>
-                        </li>
-                        <li><Link to="contact" className = 'hover-effect'>Contact</Link></li>
+                        <li><Link to="contact" className='hover-effect'>Contact</Link></li>
                     </ul>
                     <div>
-                    {/* User Actions */}
-                    <ul className='flex gap-5'>
-                        <li className='cursor-pointer hover:scale-[1.4]'><IoSearchOutline/></li>
-                        <li className='cursor-pointer hover:scale-[1.4]'><CiUser/></li>
-                        <li className='cursor-pointer hover:scale-[1.4]'><PiShoppingCartThin/></li>
-                        {/* Dark Mode */}
-                        <li className='cursor-pointer hover:scale-[1.4]'>
-                           <DarkModeToggle/> 
-                        </li>
-                    </ul>
-                </div>
+                        {/* User Actions */}
+                        <ul className='flex gap-5'>
+                            <li className='cursor-pointer hover:scale-[1.4]'><IoSearchOutline /></li>
+                            <li className='cursor-pointer hover:scale-[1.4]'><CiUser /></li>
+                            <li className='cursor-pointer hover:scale-[1.4]'><PiShoppingCartThin /></li>
+                            {/* Dark Mode */}
+                            <li className='cursor-pointer hover:scale-[1.4]'>
+                                <DarkModeToggle />
+                            </li>
+                        </ul>
+                    </div>
                 </nav>
 
             </div>
