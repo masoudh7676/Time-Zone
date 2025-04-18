04fa9c8cefe79680957cae7a04a3f250d9ce1175
Masoud Haseli <haselimasoud@gmail.com>
2025-04-19 01:59:21 +0330

    addedd photos and starting main in home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,6 +5,22 @@ export default function Home() {
   return (
     <>
     <Header/>   
+    <div className=''>
+      <div className='examples grid grid-cols-2 gap-54'>
+        <div>
+          <img src="./images/rolex1.jpg" className='w-sm' alt="" />
+        </div>
+        <div>
+          <img src="./images/citizen.jpeg" className='w-sm' alt="" />
+        </div>
+        <div>
+          <img src="./images/casio.webp" className='w-sm' alt="" />
+        </div>
+        <div>
+          <img src="./images/swatch.jpg" className='w-sm' alt="" />
+        </div>
+      </div>
+    </div>
     </>
   )
 }
