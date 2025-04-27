75cfb7949ee26af246d53b598e67bfaa58459366
Masoud Haseli <haselimasoud@gmail.com>
2025-04-27 17:42:31 +0330

    fixed alignon top sales

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,19 +2,31 @@ import React from 'react'
 import { BsBoxSeam } from "react-icons/bs";
 import { TfiUnlock } from "react-icons/tfi";
 import { FiRefreshCcw } from "react-icons/fi";
-
+import './Footer.css'
 export default function Footer() {
     return (
         <>
-            <div className=''>
+            <div className='footer w-7xl mx-auto grid grid-cols-3 gap-40 px-8 py-4 mt-40'>
                 <div>
-                    <div><BsBoxSeam /></div>
+                    <div className='text-white text-5xl'><BsBoxSeam /></div>
+                    <div className='mt-8'>
+                        <h5 className='capitalize text-white text-xl font-bold'>free shipping method</h5>
+                        <p className='text-gray-300'>aorem ixpsacdolor sit ameasecur adipisicing elitsf edasd</p>
+                    </div>
                 </div>
                 <div>
-                    <div><TfiUnlock /></div>
+                    <div className='text-white text-5xl'><TfiUnlock /></div>
+                    <div className='mt-8'>
+                        <h5 className='capitalize text-white text-xl font-bold'>Secure Payment System</h5>
+                        <p className='text-gray-'>aorem ixpsacdolor sit ameasecur adipisicing elitsf edasd.</p>
+                    </div>
                 </div>
                 <div>
-                    <div><FiRefreshCcw /></div>
+                    <div className='text-white text-5xl'><FiRefreshCcw /></div>
+                    <div className='mt-8'>
+                        <h5 className='capitalize text-white text-xl font-bold'>Secure Payment System</h5>
+                        <p className='text-gray-'>aorem ixpsacdolor sit ameasecur adipisicing elitsf edasd.</p>
+                    </div>
                 </div>
             </div>
         </>
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -9,44 +9,48 @@ export default function Home() {
     <NavBar/>
       <Header />
       <h2 className='text-center text-3xl font-bold'>top sales!</h2>
-      <div className='examples flex justify-center align-middle'>
-        <div className='grid  grid-cols-1 gap-54 sm:grid-cols-2 sm:gap-12 md:gap-52'>
-          <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <div className='examples mt-20'>
+        <div className='flex justify-around'>
+        <div>
+          <div className="max-w-sm rounded overflow-hidden shadow-lg p-6 mb-14">
             <img className="w-full transition-all hover:scale-110" src="./images/rolex1.jpg" alt="Time-Zone"/>
-              <div className="padding">
+              <div>
                 <div className="font-bold text-xl mb-2"><a href="" className='capitalize hover:text-blue-500'>click to see other watch's from this brand</a></div>
-                <p className="text-gray-700 text-base margin__top">
+                <p className="text-gray-700 text-base">
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
                 </p>
               </div>
               </div>
-              <div className="max-w-sm rounded overflow-hidden shadow-lg">
+              <div className="max-w-sm rounded overflow-hidden shadow-lg p-6">
             <img className="w-full transition-all hover:scale-110" src="./images/casio.webp" alt="Time-Zone"/>
               <div className="padding">
                 <div className="font-bold text-xl mb-2"><a href="" className='capitalize hover:text-blue-500'>click to see other watch's from this brand</a></div>
-                <p className="text-gray-700 text-base margin__top">
+                <p className="text-gray-700 text-base">
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
                 </p>
               </div>
               </div>
-              <div className="max-w-sm rounded overflow-hidden shadow-lg">
+              </div>
+              <div>
+              <div className="max-w-sm rounded overflow-hidden shadow-lg p-6 mb-14">
             <img className="w-full transition-all hover:scale-110" src="./images/swatch.jpg" alt="Time-Zone"/>
-              <div className="padding">
+              <div>
                 <div className="font-bold text-xl mb-2"><a href="" className='capitalize hover:text-blue-500'>click to see other watch's from this brand</a></div>
-                <p className="text-gray-700 text-base margin__top">
+                <p className="text-gray-700 text-base">
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
                 </p>
               </div>
               </div>
-              <div className="max-w-sm rounded overflow-hidden shadow-lg">
+              <div className="max-w-sm rounded overflow-hidden shadow-lg p-6">
             <img className="w-full transition-all hover:scale-110" src="./images/citizen.jpeg" alt="Time-Zone"/>
-              <div className="padding">
+              <div>
                 <div className="font-bold text-xl mb-2"><a href="" className='capitalize hover:text-blue-500'>click to see other watch's from this brand</a></div>
-                <p className="text-gray-700 text-base margin__top">
+                <p className="text-gray-700 text-base">
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
                 </p>
               </div>
               </div>
+          </div>
           </div>
         </div>
         <Footer/>
