2b2be69dae2e2781a6003d53012c9ada77fe0fc9
Masoud Haseli <haselimasoud@gmail.com>
2025-04-29 00:31:04 +0330

    btn style

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -10,14 +10,15 @@ export default function Contact() {
         <h4>contact</h4>
       </div>
     </div>
-    <div className='mt-50'>
-      <h5>get in touch</h5>
+    
+    <div className='mt-50 flex justify-center'>
+    <h5 className=' text-center'>get in touch</h5>
       <form className='flex flex-col'>
         <textarea name="" id="" rows={15} cols={20} className='w-72 border border-gray-300' placeholder='Enter message'></textarea>
         <input type="text" className='w-50 border border-gray-300' placeholder='Enter your name'/>
         <input type="email" className='w-50 border border-gray-300' placeholder='Email'/>
         <input type="text" className='w-100 border border-gray-300' placeholder='Subject'/>
-        <button type='submit'>SEND</button>
+        <button type='submit' className='bg-red-500 cursor-pointer text-white hover:text-black w-25 rounded-md p-2 mt-6'>SEND</button>
       </form>
     </div>
     </>
