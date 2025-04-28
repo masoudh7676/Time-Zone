f5cd771cf73f96c9a685f03444035ab36a7408cc
Masoud Haseli <haselimasoud@gmail.com>
2025-04-29 01:10:01 +0330

    creating watch data

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import NavBar from '../NavBar/NavBar'
 
 export default function Shop() {
   return (
-    <div>Shop</div>
+    <>
+      <NavBar/>
+      <div className='relative top-24 -z-50'>
+      <img src="/images/about_hero.png.webp" alt="" className=' w-full h-full'/>
+      <div className='absolute top-2/5 text-center text-7xl font-black w-full'>
+        <h4 className='capitalize'>watch shop</h4>
+      </div>
+    </div>
+    </>
   )
 }
