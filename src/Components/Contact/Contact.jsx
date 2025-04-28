98662aa9032c43b789b80bc06936a064e5f53085
Masoud Haseli <haselimasoud@gmail.com>
2025-04-28 21:33:49 +0330

    finished descktop contact top section

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -4,6 +4,12 @@ export default function Contact() {
   return (
     <>
     <NavBar/>
+    <div className='relative top-24'>
+      <img src="/images/about_hero.png.webp" alt="" className=' w-full h-full'/>
+      <div className='absolute top-2/5 text-center text-7xl font-black w-full'>
+        <h4>contact</h4>
+      </div>
+    </div>
     </>
   )
 }
