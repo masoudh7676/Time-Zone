3850efabf7824aac8c890d3f4c9167e06b5a487c
Masoud Haseli <haselimasoud@gmail.com>
2025-04-30 19:58:48 +0330

    adding watch data

--- src/watch's.jsx
+++ src/watch's.jsx
@@ -1,3 +1,13 @@
-const watchData = {
-    
-}
+let watchData = [
+    {id:1, title: "Samsung", price: 540, description: "Samsung Galaxy Watch 7 40mm" , src: "/images/Galaxy Watch 7 40mm.webp"},
+    {id:2, title: "Samsung", price: 623, description: "Samsung Galaxy Watch6 44mm" , src: "/images/Galaxy Watch6 44mm.webp"},
+    {id:3, title: "Samsung", price: 623, description: "Samsung Galaxy Watch6 Classic Astro Edition 47mm" , src: "/images/Galaxy Watch6 Classic Astro Edition 47mm.webp"},
+    {id:4, title: "Apple", price: 712, description: "Apple SE 2022 Aluminum Case 44mm" , src: "/images/Apple SE 2022 Aluminum Case 44mm.webp"},
+    {id:5, title: "Apple", price: 471, description: "Apple SE 2022 Aluminum Case 44mm" , src: "/images/Apple SE Aluminum Case 40mm.webp"},
+    {id:6, title: "Casio", price: 223, description: "Casio Analog Watch" , src: "/images/casio.webp"},
+    {id:7, title: "Casio", price: 223, description: "Casio Analog Watch" , src: "/images/casio2.webp"},
+    {id:8, title: "Casio", price: 285, description: "Casio Analog Watch" , src: "/images/casio3.webp"},
+    {id:9, title: "Casio", price: 300, description: "Casio Analog Watch" , src: "/images/casio4.webp"},
+]
+
+export default watchData
