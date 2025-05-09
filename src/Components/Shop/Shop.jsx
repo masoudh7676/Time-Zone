d93e209638fe03a4c05af7ae3cf2cc0c8c189d1a
Masoud Haseli <haselimasoud@gmail.com>
2025-05-09 20:48:25 +0330

    responsive design done in shop page

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react'
 import { Slider } from '../ui/slider'
 import { Button } from '../ui/button'
 import { Check, ChevronsUpDown } from "lucide-react"
- 
+
 import { cn } from "@/lib/utils"
 import {
   Command,
@@ -20,12 +20,13 @@ import {
 import NavBar from '../NavBar/NavBar'
 import AllProductsContext from '../../Context/Products'
 import ScrollUp from '../ScrollUp/ScrollUp'
+import Footer from '../Footer/Footer'
 
 const min = 0;
 const max = 1000;
 export default function Shop() {
   const contextData = useContext(AllProductsContext)      // watch data
-  const [values, setValues] = useState([min, max])          
+  const [values, setValues] = useState([min, max])
   const [filteredItems, setFilteredItems] = useState(contextData.items) //creating filter price
 
   const handleFilter = () => {            //price filter function
@@ -111,94 +112,95 @@ export default function Shop() {
           <h4 className='capitalize'>watch shop</h4>
         </div>
       </div>
-      <div className='flex content-center'>
-      <aside className='mt-30 w-[30%] p-10 flex flex-col items-center'>
-      <div className='sticky top-35 w-full'>            
-      <div className='w-full'>
-      <div className='flex items-center'>
-      <h5 className='text-3xl'>Price: </h5>
-      <p className='mx-2 text-2xl'> ${values[0]} - ${values[1]}</p>
-      </div>
-      <Slider
-        onValueChange={setValues}
-        value={values}
-        min={min}
-        max={max}
-        step={1}
-        className={' my-8'}
-      />
-      <div className='flex flex-col'>
-      <p className='text-xl my-4'>Current Value: ${values[1] - values[0]}</p>
+      <div className='sm:block xl:flex content-center'>
+        <aside className=' mt-30 p-10 flex flex-col items-center sm:w-120 -mb-35 mx-auto xl:mx-0 xl:w-[30%]'>
+          <div className='w-full sm:static xl:sticky top-35 '>
+            <div className='w-full'>
+              <div className='flex items-center'>
+                <h5 className='text-3xl'>Price: </h5>
+                <p className='mx-2 text-2xl'> ${values[0]} - ${values[1]}</p>
+              </div>
+              <Slider
+                onValueChange={setValues}
+                value={values}
+                min={min}
+                max={max}
+                step={1}
+                className={' my-8'}
+              />
+              <div className='flex flex-col'>
+                <p className='text-xl my-4'>Current Value: ${values[1] - values[0]}</p>
 
-      <Button variant="outline" className={'text-[18px]'} onClick={handleFilter}>Filter</Button>
-      </div>
-      </div>
-      </div> 
-      <div className=' mt-10 sticky top-95 w-full'>
-      <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          role="combobox"
-          aria-expanded={open}
-          className="w-[200px] justify-between"
-        >
-          {value
-            ? category.find((framework) => framework.value === value)?.label
-            : "Select Brand or Watch Type..."}
-          <ChevronsUpDown className="opacity-50" />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
-        <Command>
-          <CommandInput placeholder="Search Watch..." className="h-9" />
-          <CommandList>
-            <CommandEmpty>No framework found.</CommandEmpty>
-            <CommandGroup>
-              {category.map((framework) => (
-                <CommandItem
-                  key={framework.value}
-                  value={framework.value}
-                  onSelect={(currentValue) => {
-                    setValue(currentValue === value ? "" : currentValue)
-                    filterByCategory(currentValue === value ? "All Watch's" : currentValue)
-                    setOpen(false)
-                  }}
+                <Button variant="outline" className={'text-[18px]'} onClick={handleFilter}>Filter</Button>
+              </div>
+            </div>
+          </div>
+          <div className=' mt-10 sticky top-95 w-full'>
+            <Popover open={open} onOpenChange={setOpen}>
+              <PopoverTrigger asChild>
+                <Button
+                  variant="outline"
+                  role="combobox"
+                  aria-expanded={open}
+                  className="w-[200px] justify-between"
                 >
-                  {framework.label}
-                  <Check
-                    className={cn(
-                      "ml-auto",
-                      value === framework.value ? "opacity-100" : "opacity-0"
-                    )}
-                  />
-                </CommandItem>
-              ))}
-            </CommandGroup>
-          </CommandList>
-        </Command>
-      </PopoverContent>
-    </Popover>
-      </div>
-      </aside>
-      <div className='parent w-[52%]  mt-50 mb-16 grid grid-cols-3 bg-gray-100 border border-gray-200'>
-      {
-        filteredItems.map(data => (
-          <div className=' group border p-2 border-gray-200 cursor-pointer' key={data.id}>
-          <div className=' bg-white shadow-2xl rounded-4xl p-3 mb-8'>
-            <img src= {data.src} className='object-cover' alt="" />
-            <button className='text-center hidden group-hover:block w-full rounded-b-xl cursor-pointer text-white p-1 hover:text-blue-200 bg-[#ff203c]'>add to cart</button>
+                  {value
+                    ? category.find((framework) => framework.value === value)?.label
+                    : "Select Brand or Watch Type..."}
+                  <ChevronsUpDown className="opacity-50" />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="w-[200px] p-0">
+                <Command>
+                  <CommandInput placeholder="Search Watch..." className="h-9" />
+                  <CommandList>
+                    <CommandEmpty>No framework found.</CommandEmpty>
+                    <CommandGroup>
+                      {category.map((framework) => (
+                        <CommandItem
+                          key={framework.value}
+                          value={framework.value}
+                          onSelect={(currentValue) => {
+                            setValue(currentValue === value ? "" : currentValue)
+                            filterByCategory(currentValue === value ? "All Watch's" : currentValue)
+                            setOpen(false)
+                          }}
+                        >
+                          {framework.label}
+                          <Check
+                            className={cn(
+                              "ml-auto",
+                              value === framework.value ? "opacity-100" : "opacity-0"
+                            )}
+                          />
+                        </CommandItem>
+                      ))}
+                    </CommandGroup>
+                  </CommandList>
+                </Command>
+              </PopoverContent>
+            </Popover>
           </div>
-          <div className='text-center'>
-            <h4>Brand: {data.title}</h4>
-            <p>{data.description}</p>
-            <span>Price: {data.price}$</span>
-          </div>
+        </aside>
+        <div className='parent mt-50 mb-16 grid grid-cols-3 sm:w-[80%] mx-auto xl:w-[52%] xl:mx-0 bg-gray-100 border border-gray-200'>
+          {
+            filteredItems.map(data => (
+              <div className=' group border p-2 border-gray-200 cursor-pointer' key={data.id}>
+                <div className=' bg-white shadow-2xl rounded-4xl p-3 mb-8'>
+                  <img src={data.src} className='object-cover' alt="" />
+                  <button className='text-center hidden group-hover:block w-full rounded-b-xl cursor-pointer text-white p-1 hover:text-blue-200 bg-[#ff203c]'>add to cart</button>
+                </div>
+                <div className='text-center'>
+                  <h4>Brand: {data.title}</h4>
+                  <p>{data.description}</p>
+                  <span>Price: {data.price}$</span>
+                </div>
+              </div>
+            ))
+          }
         </div>
-        ))
-      }
       </div>
-      </div>
+      <Footer/>
       <ScrollUp />
     </>
   )
