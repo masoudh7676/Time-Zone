cddc11bb0ffa6dd0a66e46a823c21bd909a866ab
Masoud Haseli <haselimasoud@gmail.com>
2025-05-10 21:55:54 +0330

    resposive design for header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import './Header.css'
 export default function Header() {
   return (
-    <div className=' bg-[#f0f0f2] w-full'>
-    <div className='flex items-center justify-center'>
-        <div className='text-description  w-[50%]'>
-            <h3 className='capitalize large-text text-[90px] font-bold'>
+    <div className=' bg-[#f0f0f2] w-full mt-27'>
+    <div className='flex items-center content-between justify-center'>
+        <div className='text-description  w-[50%] mt-10 md:mt-0'>
+            <h3 className='capitalize text-3xl large-text xl:text-[90px] sm:text-6xl font-bold'>
                 select your new perfect style
             </h3>
-            <p className='text-[19px] leading-10'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, sapiente. Aut vero aperiam voluptatibus culpa laudantium assumenda dolor!</p>
-            <button className='bg-[#4a4a4b] p-[17px] px-[35px] rounded-2xl cursor-pointer hover:bg-[#ff203c] text-white mt-7 capitalize font-bold'>Shop Now</button>
+            <p className='text-[15px] md:text-[19px] leading-10'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, sapiente. Aut vero aperiam voluptatibus culpa laudantium assumenda dolor!</p>
+            <button className='bg-[#4a4a4b] mb-10 sm:mb-0 p-[7px] md:p-4 md:px-[10px] lg:p-[12px] lg:px-[20px] xl:p-[17px] px-[35px] rounded-2xl cursor-pointer hover:bg-[#ff203c] text-white mt-7 capitalize font-bold'>Shop Now</button>
         </div>
-        <div>
-                <img src="/images/watch.png.webp" className='w-[40%] sm:w-full' alt="Time-Zone" />
+        <div className='hidden sm:block w-[20%]'>
+                <img src="/images/watch.png.webp" className='w-full xl:w-full' alt="Time-Zone" />
             </div>
             </div>
     </div>
