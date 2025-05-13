8fa357ceaf3965ad39f141d5913f06c465f39bbb
Masoud Haseli <haselimasoud@gmail.com>
2025-05-13 18:58:31 +0330

    added table

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,6 +1,5 @@
-import React, { useContext } from 'react'
+  import React, { useContext } from 'react'
 import NavBar from '../NavBar/NavBar'
-import AllProductsContext from '@/Context/Products'
 
 
 // const contextData = useContext(AllProductsContext)
@@ -9,16 +8,31 @@ export default function Cart() {
     <>
       <NavBar/>
       <div className='mt-28 relative'>
-      <img src="/images/about_hero.png.webp" alt=""/>
-        <h4 className='absolute top-2/5 font-bold text-7xl left-2/5'>CheckOut</h4>
+        <img src="/images/about_hero.png.webp" alt=""/>
+        <h4 className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-bold text-7xl'>CheckOut</h4>
       </div>
-      <div className='mt-25 flex flex-row-reverse w-[65%] mx-auto'>
-        <div className='border-t-1 border-t-gray-300 border-b-1 border-b-gray-300 w-[60%] p-5 flex flex-row-reverse'>
-          <div>
-            <button className='font-bold'>X</button>
-          </div>
-        </div>
-        <aside></aside>
+      <div className='mt-12 w-full mx-auto flex justify-center'>
+        <table className='table-auto w-1/2 border border-gray-300'>
+          <thead className='border-b border-gray-300 bg-gray-100'>
+            <tr>
+              <th className='px-4 py-2 text-left'>Product</th>
+              <th className='px-4 py-2 text-center'>Quantity</th>
+              <th className='px-4 py-2 text-right'>Price</th>
+              <th className='px-4 py-2 text-right'>Total Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className='border-b border-gray-200'>
+              <td className='px-4 py-2 flex items-center gap-2'>
+                <img src="/images/casio.webp" className='w-16 h-auto' alt="Casio" />
+                Casio
+              </td>
+              <td className='px-4 py-2 text-center'>4</td>
+              <td className='px-4 py-2 text-right'>$1235</td>
+              <td className='px-4 py-2 text-right'>$4940</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </>
   )
