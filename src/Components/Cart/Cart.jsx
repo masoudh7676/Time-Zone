8cb457feb8d1afa9ebb8caa48f0c1ed843697918
Masoud Haseli <haselimasoud@gmail.com>
2025-05-13 21:49:30 +0330

    finished aside

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,28 +1,48 @@
-  import React, { useContext } from 'react'
+import React, { useContext } from 'react'
 import NavBar from '../NavBar/NavBar'
-
+import { Label } from "@/components/ui/label"
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+import Footer from '../Footer/Footer'
 
 // const contextData = useContext(AllProductsContext)
 export default function Cart() {
   return (
     <>
-      <NavBar/>
+      <NavBar />
       <div className='mt-28 relative'>
-        <img src="/images/about_hero.png.webp" alt=""/>
+        <img src="/images/about_hero.png.webp" alt="" />
         <h4 className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-bold text-7xl'>CheckOut</h4>
       </div>
-      
-      <div className='mt-12 w-full mx-auto flex justify-center'>
-              <aside className='shadow-2xl rounded-2xl mr-10 w-2/7 text-center p-7'>
-          <h3 className='mb-4'>
-            Total Cart Price
-          </h3>
-          <h5 className='mb-4'>Total price:454</h5>
-          <div>
-            <button className='cursor-pointer w-[70%] p-3 rounded-2xl bg-gray-200'>Proceed To payment</button>
-          </div>
+
+      <div className='mt-12 w-full flex justify-center content-center'>
+        <aside className='shadow-2xl rounded-2xl mr-10 w-2/7 p-7 flex flex-col text-center'>
+          <h5 className='mb-4 text-2xl'>Total Price: 454$</h5>
+
+          <h6>Sending method:</h6>
+          <form method='post'>
+            <div className='flex flex-col'>
+            <div className='flex w-full justify-center content-center mt-4'>
+              <input type="radio" className='mr-3' name='send' id='1' />
+              <label htmlFor='1'>Post</label>
+              </div>
+              <div className='flex w-full justify-center content-center mt-4 ml-2'>
+              <input type="radio" className='mr-3' name='send' id='2' />
+              <label htmlFor='2'>Person</label>
+              </div>
+              <div className='flex w-full justify-center content-center my-4'>
+              <input type="radio" className='mr-3' name='send' id='3' />
+              <label htmlFor='3'>Bike</label>
+              </div>
+            </div>
+            <div>
+              <a href="#">
+                <button className='cursor-pointer w-[70%] p-3 hover:scale-110 hover:text-blue-600 rounded-2xl text-xl bg-gray-200'>Proceed To payment</button>
+              </a>
+            </div>
+          </form>
+
         </aside>
-        <table className='table-auto w-1/2 border border-gray-300'>
+        <table className='table-auto w-1/2 border border-gray-300 h-1'>
           <thead className='border-b border-gray-300 bg-gray-100'>
             <tr>
               <th className='px-4 py-2 text-left'>Product</th>
@@ -44,6 +64,7 @@ export default function Cart() {
           </tbody>
         </table>
       </div>
+      <Footer/>
     </>
   )
 }
