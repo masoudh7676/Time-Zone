196b203c670fc80e973492c496f19fde59a1aa00
Masoud Haseli <haselimasoud@gmail.com>
2025-05-13 19:11:55 +0330

    added aside

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -11,7 +11,17 @@ export default function Cart() {
         <img src="/images/about_hero.png.webp" alt=""/>
         <h4 className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-bold text-7xl'>CheckOut</h4>
       </div>
+      
       <div className='mt-12 w-full mx-auto flex justify-center'>
+              <aside className='shadow-2xl rounded-2xl mr-10 w-2/7 text-center p-7'>
+          <h3 className='mb-4'>
+            Total Cart Price
+          </h3>
+          <h5 className='mb-4'>Total price:454</h5>
+          <div>
+            <button className='cursor-pointer w-[70%] p-3 rounded-2xl bg-gray-200'>Proceed To payment</button>
+          </div>
+        </aside>
         <table className='table-auto w-1/2 border border-gray-300'>
           <thead className='border-b border-gray-300 bg-gray-100'>
             <tr>
