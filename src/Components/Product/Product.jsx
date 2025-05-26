8f15b089a024cd0731976058f18312d870faad37
Masoud Haseli <haselimasoud@gmail.com>
2025-05-26 20:24:20 +0330

    fixed some bugs

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -12,16 +12,18 @@ import {
 export default function Product() {
   return (
     <>
-      <div className='w-[30%] md:w-[60%] lg:w-[40%] mx-auto my-10 p-5 border border-gray-300 rounded-lg shadow-lg'>
+      <div className='w-[60%] md:w-[50%] lg:w-[30%] mx-auto my-10 p-5 border border-gray-300 rounded-lg shadow-lg'>
         <aside>
           <h5 className='font-bold'>price : $</h5>
           <ul>
             <li>
-              <Accordion type="single" collapsible className="w-full flex items-center">
-                <CiStar className='mr-5' />
+              <Accordion type="single" collapsible className="w-full flex items-center">      
                 <AccordionItem className={'w-full'} value="item-1">
-                  <AccordionTrigger>Guarantee</AccordionTrigger>
-                  <AccordionContent>
+                <div className='flex items-center'>
+                <CiStar className='mr-5' />
+                  <AccordionTrigger className={'text-xl'}>Guarantee</AccordionTrigger>
+                  </div>
+                  <AccordionContent className={'leading-8'}>
                     life time guarantee
                   </AccordionContent>
                 </AccordionItem>
@@ -30,20 +32,25 @@ export default function Product() {
             </li>
             <li>
               <Accordion type="single" collapsible className="w-full flex items-center ">
-                <FaTruckFast className='mr-5' />
                 <AccordionItem className={'w-full'} value="item-1">
-                  <AccordionTrigger>Fast Delivery</AccordionTrigger>
-                  <AccordionContent>
+                  <div className='flex items-center'>
+                    <FaTruckFast className='mr-5' />
+                    <AccordionTrigger className={'text-xl'}>Fast Delivery</AccordionTrigger>
+
+                  </div>
+                  <AccordionContent className={'leading-8'}>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo temporibus, dolores modi ad odio dolor assumenda qui veritatis, libero amet dicta dolorem ullam quod error a voluptates doloremque deserunt porro
                   </AccordionContent>
                 </AccordionItem>
               </Accordion>
             </li>
             <Accordion type="single" collapsible className="w-full flex items-center ">
-              <FaRegCreditCard className='mr-5' />
               <AccordionItem className={'w-full'} value="item-1">
-                <AccordionTrigger>Secure Payment</AccordionTrigger>
-                <AccordionContent>
+                <div className='flex items-center'>
+                <FaRegCreditCard className='mr-5' />
+                  <AccordionTrigger className={'text-xl'}>Secure Payment</AccordionTrigger>
+                </div>
+                <AccordionContent className={'leading-8'}>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo temporibus, dolores modi ad odio dolor assumenda qui veritatis, libero amet dicta dolorem ullam quod error a voluptates doloremque deserunt porro
                 </AccordionContent>
               </AccordionItem>
