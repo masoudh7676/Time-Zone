c9e58d1f5dcd9a2d71615f00a53f525519c4f01f
Masoud Haseli <haselimasoud@gmail.com>
2025-05-26 18:34:07 +0330

    product details start

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
-
+import { CiStar } from "react-icons/ci";
 export default function Product() {
   return (
-    <div>Product</div>
+    <>
+      <div>
+        <aside>
+          <h5 className='font-bold'>price : $</h5>
+          <ul>
+            <li>
+              <span>guarantee</span>
+                <CiStar />
+            </li>
+            <li>
+      
+            </li>
+            <li>
+
+            </li>
+            <li>
+
+            </li>
+          </ul>
+        </aside>
+      </div>
+    </>
   )
 }
