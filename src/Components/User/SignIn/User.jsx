eee253807ceccc6e6e4b5fa9389e8fd08b87c765
Masoud Haseli <haselimasoud@gmail.com>
2025-05-31 17:50:18 +0330

    added meta tags and started formik for signIn

--- src/Components/User/SignIn/User.jsx
+++ src/Components/User/SignIn/User.jsx
@@ -1,19 +1,24 @@
 import NavBar from '@/Components/NavBar/NavBar'
+import { Form, Formik } from 'formik'
 import React from 'react'
 import { Link } from 'react-router'
 export default function User() {
   return (
     <>
     <NavBar/>   
-<div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
+      <Formik initialValues={{email: "email", password: ""}}>
+        {(values) => (
+                <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
   <div className="max-w-md w-full bg-white rounded-xl shadow-lg p-8">
     <h2 className="title font-bold text-gray-900 mb-6 text-center">Sign In</h2>
     
-    <form className="space-y-4">
+    <Form className="space-y-4">
       <div className='margin__bottom'>
         <label className="block text-sm font-medium text-gray-700 mb-1">Email</label>
         <input 
           type="email" 
+          name='email'
+          value={values.email}
           className="w-full px-4 py-2 !important border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 outline-none transition-all"
           placeholder="your@email.com"
         />
@@ -23,6 +28,8 @@ export default function User() {
         <label className="block text-sm font-medium text-gray-700 mb__1">Password</label>
         <input 
           type="password" 
+          name='password'
+          value={values.password}
           className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 outline-none transition-all"
           placeholder="••••••••"
         />
@@ -39,7 +46,7 @@ export default function User() {
       <button className="w-full bg-indigo-600 hover:bg-indigo-700 text-white font-medium padding rounded-lg transition-colors">
         Sign In
       </button>
-    </form>
+    </Form>
 
     <div className="mt-6 text-center text-sm text-gray-600">
       Don't have an account? 
@@ -47,6 +54,8 @@ export default function User() {
     </div>
   </div>
 </div>
+        )}
+      </Formik>
     </>
   )
 }
