011cb22d041a0eb1c4a0d85ccb79dc0826f62c90
Masoud Haseli <haselimasoud@gmail.com>
2025-06-06 23:11:43 +0330

    fixing deploy bug

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx'
 
 const root = ReactDOM.createRoot(document.querySelector('body'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/Time-Zone/">
     <App/>
   </BrowserRouter>
 )
